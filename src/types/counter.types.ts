5fe3527a905f9a956c19106cbc2f5da38675e0a7
Gih2139Sanches <giovanaclemente2139@gmail.com>
2026-02-24 18:07:50 -0300

    :sparkles: feat: adicionando types para themes

---
+++ src/types/counter.types.ts
@@ -0,0 +1,8 @@
+export type AccentColor = 'coral' | 'cyan' | 'purple';
+
+export type ThemeMode = 'dark' | 'light';
+
+export interface CounterState {
+  count: number;
+  isTimerRunning: boolean;
+}
