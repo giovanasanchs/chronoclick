e3793bbe3439a530c26252abc15de5e4eae96246
Gih2139Sanches <giovanaclemente2139@gmail.com>
2026-02-25 13:20:42 -0300

    :recycle: refactor: mudando root para routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../app/App";
-import NotFound from "../pages/NotFound";
 
-const Root = () => (
+const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
 
-export default Root;
+export default Router;
