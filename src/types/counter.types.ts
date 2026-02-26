e3adb3530d2be23c0f2625cbf6e7574f4d857d8d
Gih2139Sanches <giovanaclemente2139@gmail.com>
2026-02-25 23:42:11 -0300

    :sparkles: feat: adicionando novo type

--- src/types/counter.types.ts
+++ src/types/counter.types.ts
@@ -1,6 +1,8 @@
-export type AccentColor = 'coral' | 'cyan' | 'purple';
+export type AccentColor = "coral" | "cyan" | "purple";
 
-export type ThemeMode = 'dark' | 'light';
+export type ThemeMode = "dark" | "light";
+
+export type Mode = "counter" | "timer";
 
 export interface CounterState {
   count: number;
